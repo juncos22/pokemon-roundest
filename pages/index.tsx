57ba5e7865d7ab9bf96257a0517aed572166ef17
juncos22 <juncosnicolas36@gmail.com>
2023-01-10 00:26:03 -0300

    UI creation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
     <>
-      <h3 className="text-2xl text-red-200">Main page</h3>
+      <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <div className="text-2xl text-center">
+          Which Pokemon is Roundest?
+        </div>
+        <div className="p-2" />
+        <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+          <div className="w-16 h-16 bg-red-200"></div>
+          <div className="p-8">Vs.</div>
+          <div className="w-16 h-16 bg-red-200"></div>
+        </div>
+      </div>
     </>
   )
 }
