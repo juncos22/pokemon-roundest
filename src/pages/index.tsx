f637e3ac840ff4085b2f3cf778c50c0cfe020f56
juncos22 <juncosnicolas36@gmail.com>
2023-01-10 02:08:39 -0300

    pokemon data in backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,39 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon"
 import { trpc } from "@/utils/trpc"
+import { useMemo, useState } from "react"
 
 export default function Home() {
-  const { data, isLoading } = trpc.hello.useQuery({ text: 'Nicolas' })
-  if (isLoading) return <div>Loading...</div>
+  const [ids, setIds] = useState(() => getOptionsForVote())
+  const [first, second] = ids
+  const firstPokemon = trpc.getPokemonById.useQuery({ id: first })
+  const secondPokemon = trpc.getPokemonById.useQuery({ id: second })
+
   return (
     <>
-      {data?.greeting}
-      {/* <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="h-screen w-screen flex flex-col justify-center items-center">
         <div className="text-2xl text-center">
           Which Pokemon is Roundest?
         </div>
         <div className="p-2" />
         <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-          <div className="w-16 h-16 bg-red-200"></div>
+          <div className="w-64 h-64 flex flex-col">{firstPokemon.isLoading ? 'Loading Pokemon... ' : (
+            <img src={firstPokemon.data?.sprites.front_default as string} alt={firstPokemon.data?.name} className="w-full" />
+          )}
+            <div className="text-xl text-center capitalize mt-[-2rem]">
+              {firstPokemon.data?.name}
+            </div>
+          </div>
           <div className="p-8">Vs.</div>
-          <div className="w-16 h-16 bg-red-200"></div>
+          <div className="w-64 h-64 flex flex-col">{secondPokemon.isLoading ? 'Loading Pokemon...' : (
+            <img src={secondPokemon.data?.sprites.front_default as string} alt={secondPokemon.data?.name} className="w-full" />
+          )}
+            <div className="text-xl text-center capitalize mt-[-2rem]">
+              {secondPokemon.data?.name}
+            </div>
+          </div>
+          <div className="p-2" />
         </div>
-      </div> */}
+      </div>
     </>
   )
 }
